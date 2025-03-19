a421ac1fdb251dd1279c6ef332f8dd31226b97b1
Iliya Kushnir <iliyakyshnir07@gmail.com>
2025-03-19 23:25:37 +0200

    changed clocks on exp page

--- ClocksProj/src/ExperiencePage/GridImg/GridImg.jsx
+++ ClocksProj/src/ExperiencePage/GridImg/GridImg.jsx
@@ -3,14 +3,42 @@ import styles from "./GridImg.module.scss";
 
 const GridLayout = () => {
 
-    return (
-        <>
-            <div className={styles.gridContainer}>
+    /*
+                <div className={styles.gridContainer}>
                 <div className={styles.gridItem1}><img className={styles.img} src="/compressed/RoundWatch.png" alt="" /></div>
                 <div className={styles.gridItem3}><img className={styles.img} src="/compressed/SmallWatch.png" alt="" /></div>
                 <div className={styles.gridItem4}><img className={styles.img} src="/compressed/SmallWatch2.png" alt="" /></div>
                 <div className={styles.gridItem2}><img className={styles.img} src="/compressed/BigWatch.png" alt="" /></div>
             </div>
+            <div className={styles.restImgs}>
+                <div>
+
+                </div>
+                <div>
+                    
+                </div>
+                <div>
+
+                </div>
+            </div>
+    */
+
+    return (
+        <>
+            <div class={styles.worksGrid}>
+                <img src="/compressed/RoundWatch.png" className={styles.wide}/>
+                <img src="/images/light.png" className={styles.small} />
+                <img src="/compressed/SmallWatch2.png" className={styles.small} />
+                <img src="/compressed/BigWatch.png" className={styles.wide} />
+                <img src="/images/bigWatch2.png" className={styles.wide} />
+                <img src="/images/vostok.png" className={styles.small} />
+                <img src="/images/alarm.png" className={styles.small} />
+                <img src="/images/lighthouse.png" className={styles.small} />
+                <img src="/compressed/SmallWatch.png" className={styles.small}  />
+                <img src="/images/bigWatch3.png" className={styles.wide} />
+            </div>
+
+
         </>
     )
 }
