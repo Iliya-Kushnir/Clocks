e84d6cb92cb45d533ffeb94108df27776a1dc3a1
Iliya Kushnir <iliyakyshnir07@gmail.com>
2025-03-24 19:43:30 +0200

    second attempt to fix router

--- ClocksProj/vite.config.js
+++ ClocksProj/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/", // Убедись, что base установлен корректно
   plugins: [react()],
+  server: {
+    historyApiFallback: true // Для корректной работы React Router
+  },
+  build: {
+    outDir: "dist" // Убедись, что билд идёт в dist
+  }
 })
