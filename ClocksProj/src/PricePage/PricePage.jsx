bad39c04acd48a0e6c62dfa957fcef71d559e8e1
Iliya Kushnir <iliyakyshnir07@gmail.com>
2025-03-24 20:13:20 +0200

    final fixes (I hope so)

--- ClocksProj/src/PricePage/PricePage.jsx
+++ ClocksProj/src/PricePage/PricePage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef} from "react";
 import Header from "../components/Header/Header.jsx";
 import Banner from "../components/Banner/Banner.jsx";
 import MapSection from "../components/MapSection/MapSection.jsx";
@@ -9,6 +9,7 @@ import { useNavigate } from "react-router";
 import { useLanguage } from "../LanguageContext/LanguageContext.jsx";
 
 const PricePage = () => {
+  const section2Ref = useRef()
   const {t} = useLanguage()
   const handleClick1 = () => navigate("/DeliveryPage");
   const handleClick2 = () => navigate("/PriceList");
